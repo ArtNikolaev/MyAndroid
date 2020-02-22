c2998bb06f7f1cda0ea15558d5370be0254224df
ArtNikolaev <a6311633@gmail.com>
2020-02-22 21:28:07 +0300

    commit

---
+++ app/src/main/java/by/tms/myandroid/Dz1Activity.kt
@@ -0,0 +1,14 @@
+package by.tms.myandroid
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class Dz1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz1)
+    }
+}
--- app/src/main/java/by/tms/myandroid/MainActivity.kt
+++ app/src/main/java/by/tms/myandroid/MainActivity.kt
@@ -1,12 +1,26 @@
 package by.tms.myandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun Click(view: View) {
+        val Clic = Intent(this, Dz1Activity::class.java)
+        startActivity(Clic)
+    }
+
 }
