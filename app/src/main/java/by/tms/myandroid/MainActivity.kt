295edb084d0514ac950362203a0f4b1731b5db7a
ArtNikolaev <59572650+ArtNikolaev@users.noreply.github.com>
2020-02-24 14:43:24 +0300

    Add files via upload

---
+++ app/src/main/java/by/tms/myandroid/MainActivity.kt
@@ -0,0 +1,24 @@
+package by.tms.myandroid
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonHW1 = button
+        buttonHW1.setOnClickListener{ сlick(buttonHW1)}
+    }
+
+    fun сlick(view: View) {
+        val intent = Intent(this, Dz1Activity::class.java)
+        startActivity(intent)
+    }
+
+}
